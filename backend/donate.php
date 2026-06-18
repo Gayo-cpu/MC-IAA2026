8280cda7836e55dc324459556638b5ce2a9e5631
hajjboy04 <hajjmwinyimsa04@gmail.com>
2026-06-18 04:21:51 +0300

    database connection and donation backend

---
+++ backend/donate.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../config/db.php";
+
+function security($defence){
+    $defence = htmlspecialchars($defence);
+    $defence = trim($defence);
+    $defence = stripslashes($defence);
+}
+
+$category = security($_POST['category']);
+$fullName = security($_POST['full_name']);
+$email = security($_POST['email']);
+$phone = security($_POST['phone']);
+$amount = security($_POST['amount']);
+$donateType = security($_POST['donation_type']);
+$paymentMethod = security($_POST['payment_method']);
+
+if($conn){
+    if(empty($category) || empty($fullName) || empty($email) || empty($phone) || empty($amount) || empty($donateType) || empty(paymentMethod) ){
+        exit();
+    }else if(!filter_var($email, FILTER_VALIDATE_EMAIL == true)){
+        echo "<h4>Invalid email address<h4>";
+    }else{
+        $query = "INSERT INTO donate(?,?,?,?,?,?,?) VALUE($category,$fullName,$email,$phone,$amount,$donateType,$paymentMethod)";
+        $result =mysqli_query($conn, $query);
+
+    }
+}
+?>
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'mciaa';
+
+$conn = mysqli_connect($hostname,$username,$password,$db);
+?>
