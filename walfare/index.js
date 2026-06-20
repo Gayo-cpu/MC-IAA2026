2d3e706808644e2b0fd4f408b9fb62d775088664
sonia367 <sonnyshebby@gmail.com>
2026-06-21 00:55:30 +0300

    welfare dashboard

---
+++ walfare/index.js
@@ -0,0 +1,27 @@
+const logoutBtn = document.getElementById("logoutBtn");
+
+const logoutModal = document.getElementById("logoutModal");
+
+const cancelLogout = document.getElementById("cancelLogout");
+
+const confirmLogout = document.getElementById("confirmLogout");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    logoutModal.style.display = "flex";
+  });
+}
+
+if (cancelLogout) {
+  cancelLogout.addEventListener("click", function () {
+    logoutModal.style.display = "none";
+  });
+}
+
+if (confirmLogout) {
+  confirmLogout.addEventListener("click", function () {
+    window.location.href = "login.html";
+  });
+}
