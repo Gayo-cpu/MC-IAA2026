3be41091da7391a117b1dfe71a215f16baafbfc1
hajjboy04 <hajjmwinyimsa04@gmail.com>
2026-06-20 23:02:42 +0300

    donate functioning successfully

--- config/db.php
+++ config/db.php
@@ -3,11 +3,11 @@
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
-$db = 'mciaa_db';
+$db = 'mciaa';
 
 $conn = mysqli_connect($hostname,$username,$password,$db);
 
-if($conn->connection_error){
-    die ("Connection failed: ".$conn->connection_error);
+if($conn->connect_error){
+    die ("Connection failed: ".$conn->connect_error);
 }
 ?>
