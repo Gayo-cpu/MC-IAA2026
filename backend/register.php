bf6a6463ac036d0e6c576fd904bbe983008f9882
MsFinnah5000 <safinamshegheli@gmail.com>
2026-06-21 13:48:58 +0300

    marekebisho

--- backend/register.php
+++ backend/register.php
@@ -2,39 +2,38 @@
 
 require_once "../config/db.php";
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $full_name = mysqli_real_escape_string($conn,$_POST['full_name']);
-    $reg_number = mysqli_real_escape_string($conn,$_POST['reg_number']);
-    $course_name = mysqli_real_escape_string($conn,$_POST['course_name']);
-    $study_year = mysqli_real_escape_string($conn,$_POST['study_year']);
-    $contact_number = mysqli_real_escape_string($conn,$_POST['contact_number']);
-   $email = mysqli_real_escape_string($conn,$_POST['email']);
-   $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $gender = mysqli_real_escape_string($conn,$_POST['gender']);
-   
-   $sql = "INSERT INTO users(
-    fullname,
-    phonenumber,
-    email,
-    password,
-    gender,
-    registration_no,
-    academic_year
-)
-VALUES(
-    '$full_name',
-    '$contact_number',
-    '$email',
-    '$password',
-    '$gender',
-    '$reg_number',
-    '$study_year'
-)";
+    $full_name = mysqli_real_escape_string($conn, $_POST['full_name']);
+    $reg_number = mysqli_real_escape_string($conn, $_POST['reg_number']);
+    $study_year = mysqli_real_escape_string($conn, $_POST['study_year']);
+    $contact_number = mysqli_real_escape_string($conn, $_POST['contact_number']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
 
-    if(mysqli_query($conn,$sql)){
+    $sql = "INSERT INTO users(
+        fullname,
+        phonenumber,
+        email,
+        password,
+        gender,
+        registration_no,
+        academic_year
+    )
+    VALUES(
+        '$full_name',
+        '$contact_number',
+        '$email',
+        '$password',
+        '$gender',
+        '$reg_number',
+        '$study_year'
+    )";
+
+    if(mysqli_query($conn, $sql)){
         echo "Registration Successful";
-    }else{
+    } else {
         echo mysqli_error($conn);
     }
 }
