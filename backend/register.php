a52087c165d2b64d07d869b873934c22b96f9ab1
MsFinnah5000 <safinamshegheli@gmail.com>
2026-06-21 09:12:57 +0300

    update

--- backend/register.php
+++ backend/register.php
@@ -9,21 +9,28 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $course_name = mysqli_real_escape_string($conn,$_POST['course_name']);
     $study_year = mysqli_real_escape_string($conn,$_POST['study_year']);
     $contact_number = mysqli_real_escape_string($conn,$_POST['contact_number']);
-
-    $sql = "INSERT INTO users(
-        full_name,
-        reg_number,
-        course_name,
-        study_year,
-        contact_number
-    )
-    VALUES(
-        '$full_name',
-        '$reg_number',
-        '$course_name',
-        '$study_year',
-        '$contact_number'
-    )";
+   $email = mysqli_real_escape_string($conn,$_POST['email']);
+   $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $gender = mysqli_real_escape_string($conn,$_POST['gender']);
+   
+   $sql = "INSERT INTO users(
+    fullname,
+    phonenumber,
+    email,
+    password,
+    gender,
+    registration_no,
+    academic_year
+)
+VALUES(
+    '$full_name',
+    '$contact_number',
+    '$email',
+    '$password',
+    '$gender',
+    '$reg_number',
+    '$study_year'
+)";
 
     if(mysqli_query($conn,$sql)){
         echo "Registration Successful";
